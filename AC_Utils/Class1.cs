ebfdb9a0e18d56a1eb4ee44810908687b22bd3a2
Wayne Bielski <wayne.bielski@live.com>
2017-08-07 09:26:40 -0500

    Add project files.

---
+++ AC_Utils/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Autodesk.AutoCAD.Runtime;
+using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+
+
+[assembly: CommandClass(typeof(AC_Utils.Class1));
+
+namespace AC_Utils
+{
+	public class Class1
+	{
+		//Put your AutoCAD command here
+		[CommandMethod("acCommand")]
+		public void AcCommand()
+		{
+			// Get the current document and database, start a transaction
+			Document acDoc = Application.DocumentManager.MdiActiveDocument;
+			Database acCurDb = acDoc.Database;
+
+			// Start a transaction with the Transaction Manager
+			using (Transaction acTrans = acCurDb.TransactionManager.StartTransaction())
+			{
+				// Body of transaction goes here
+			}
+		}
+	}
+}
